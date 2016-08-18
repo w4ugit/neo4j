3dad0076bd846a73ed01d539b2a640913985a047
w4ugit <admin@web4u.in.ua>
2016-08-18 15:25:03 +0300

    Временный файл для тестов

---
+++ tmp.php
@@ -0,0 +1,238 @@
+Array
+(
+[0] => Everyman\Neo4j\Relationship Object
+(
+[start:protected] => Everyman\Neo4j\Node Object
+(
+[labels:protected] =>
+[id:protected] => 278
+[client:protected] => Everyman\Neo4j\Client Object
+(
+[transport:protected] => Everyman\Neo4j\Transport\Curl Object
+(
+[handle:protected] => Resource id #17
+[scheme:protected] => http
+[host:protected] => localhost
+[port:protected] => 7474
+[path:protected] => /db/data
+[username:protected] =>
+[password:protected] =>
+)
+
+[entityMapper:protected] => Everyman\Neo4j\EntityMapper Object
+(
+[client:protected] => Everyman\Neo4j\Client Object
+*RECURSION*
+)
+
+[entityCache:protected] => Everyman\Neo4j\Cache\EntityCache Object
+(
+[client:protected] => Everyman\Neo4j\Client Object
+*RECURSION*
+[cache:protected] => Everyman\Neo4j\Cache\None Object
+(
+)
+
+[cacheTimeout:protected] => 0
+)
+
+[labelCache:protected] => Everyman\Neo4j\Cache\Variable Object
+(
+[items:protected] => Array
+(
+)
+
+)
+
+[serverInfo:protected] =>
+[openBatch:protected] =>
+[nodeFactory:protected] => Closure Object
+(
+[this] => Everyman\Neo4j\Client Object
+*RECURSION*
+[parameter] => Array
+(
+[$client] =>
+[$properties] =>
+)
+
+)
+
+[relFactory:protected] => Closure Object
+(
+[this] => Everyman\Neo4j\Client Object
+*RECURSION*
+[parameter] => Array
+(
+[$client] =>
+[$properties] =>
+)
+
+)
+
+)
+
+[properties:protected] => Array
+(
+)
+
+[lazyLoad:protected] => 1
+[loaded:protected] =>
+)
+
+[end:protected] => Everyman\Neo4j\Node Object
+(
+[labels:protected] =>
+[id:protected] => 282
+[client:protected] => Everyman\Neo4j\Client Object
+(
+[transport:protected] => Everyman\Neo4j\Transport\Curl Object
+(
+[handle:protected] => Resource id #17
+[scheme:protected] => http
+[host:protected] => localhost
+[port:protected] => 7474
+[path:protected] => /db/data
+[username:protected] =>
+[password:protected] =>
+)
+
+[entityMapper:protected] => Everyman\Neo4j\EntityMapper Object
+(
+[client:protected] => Everyman\Neo4j\Client Object
+*RECURSION*
+)
+
+[entityCache:protected] => Everyman\Neo4j\Cache\EntityCache Object
+(
+[client:protected] => Everyman\Neo4j\Client Object
+*RECURSION*
+[cache:protected] => Everyman\Neo4j\Cache\None Object
+(
+)
+
+[cacheTimeout:protected] => 0
+)
+
+[labelCache:protected] => Everyman\Neo4j\Cache\Variable Object
+(
+[items:protected] => Array
+(
+)
+
+)
+
+[serverInfo:protected] =>
+[openBatch:protected] =>
+[nodeFactory:protected] => Closure Object
+(
+[this] => Everyman\Neo4j\Client Object
+*RECURSION*
+[parameter] => Array
+(
+[$client] =>
+[$properties] =>
+)
+
+)
+
+[relFactory:protected] => Closure Object
+(
+[this] => Everyman\Neo4j\Client Object
+*RECURSION*
+[parameter] => Array
+(
+[$client] =>
+[$properties] =>
+)
+
+)
+
+)
+
+[properties:protected] => Array
+(
+)
+
+[lazyLoad:protected] => 1
+[loaded:protected] =>
+)
+
+[type:protected] => IN
+[id:protected] => 77
+[client:protected] => Everyman\Neo4j\Client Object
+(
+[transport:protected] => Everyman\Neo4j\Transport\Curl Object
+(
+[handle:protected] => Resource id #17
+[scheme:protected] => http
+[host:protected] => localhost
+[port:protected] => 7474
+[path:protected] => /db/data
+[username:protected] =>
+[password:protected] =>
+)
+
+[entityMapper:protected] => Everyman\Neo4j\EntityMapper Object
+(
+[client:protected] => Everyman\Neo4j\Client Object
+*RECURSION*
+)
+
+[entityCache:protected] => Everyman\Neo4j\Cache\EntityCache Object
+(
+[client:protected] => Everyman\Neo4j\Client Object
+*RECURSION*
+[cache:protected] => Everyman\Neo4j\Cache\None Object
+(
+)
+
+[cacheTimeout:protected] => 0
+)
+
+[labelCache:protected] => Everyman\Neo4j\Cache\Variable Object
+(
+[items:protected] => Array
+(
+)
+
+)
+
+[serverInfo:protected] =>
+[openBatch:protected] =>
+[nodeFactory:protected] => Closure Object
+(
+[this] => Everyman\Neo4j\Client Object
+*RECURSION*
+[parameter] => Array
+(
+[$client] =>
+[$properties] =>
+)
+
+)
+
+[relFactory:protected] => Closure Object
+(
+[this] => Everyman\Neo4j\Client Object
+*RECURSION*
+[parameter] => Array
+(
+[$client] =>
+[$properties] =>
+)
+
+)
+
+)
+
+[properties:protected] => Array
+(
+[protecd] => false
+)
+
+[lazyLoad:protected] =>
+[loaded:protected] =>
+)
+
+)
